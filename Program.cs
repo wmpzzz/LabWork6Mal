aea1eac5555200840c3813a50abf0338a9134e8e
killk <killk@DESKTOP-L4PANLR>
2025-01-28 22:11:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(GetPower(2, 10));
+    }
+
+    public int squareOfSquare(int height = 5)
+    {
+        return height * height;
+    }
+
+    public double squareOfSquare(double height = 5)
+    {
+        return height * height;
+    }
+
+    public int squareOfRectangle(int height = 14, int width = 88)
+    {
+        return height * width;
+    }
+
+    public double squareOfRectangle(double height = 14, double width = 88)
+    {
+        return height * width;
+    }
+
+    public static double GetPower(int n)
+    {
+        double x = 2;
+        double value = x;
+
+        if (n <= 0)
+            return -1;
+
+        for (int i = 1; i < n; i++)
+            value = value * x;
+
+        return 1 / value;
+    }
+
+    public static double GetPower(double x, int n)
+    {
+        double value = x;
+
+        if (n <= 0)
+            return -1;
+
+        for (int i = 1; i < n; i++)
+            value = value * x;
+
+        return 1 / value;
+    }
+
+    public static double GetDivision()
+    {
+        double x = 10;
+        if (x == 0)
+            return -1;
+
+        return 1 / x;
+    }
+
+    public static double GetDivision(double x)
+    {
+        if (x == 0)
+            return -1;
+
+        return 1 / x;
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,73 @@
+﻿public class Worker
+{
+    public string _fullName = "unknown";
+    public string _post = "unknown";
+    public int _salary = 0;
+
+
+    public Worker() : this("Kravchuck Mikhail Sergeevich", "cleaning manager", 10000000)
+    {
+    }
+
+    public Worker(string fullName, string post, int salary)
+    {
+        FullName = fullName;
+        Post = post;
+        Salary = salary;
+    }
+
+    public string FullName
+    {
+        get => _fullName;
+        set
+        {
+            if (value != "")
+                _fullName = value;
+        }
+    }
+
+    public string Post
+    {
+        get => _post;
+        set
+        {
+            if (value != "")
+                _post = value;
+        }
+    }
+
+    public int Salary
+    {
+        get => _salary;
+        set
+        {
+            if (value >= 0)
+                _salary = value;
+        }
+    }
+
+    public object this[string index]
+    {
+        get
+        {
+            if (index == "fullName")
+                return _fullName;
+            if (index == "post")
+                return _post;
+            if (index == "salary")
+                return _salary;
+            return null;
+        }
+    }
+
+    public char this[int index]
+    {
+        get
+        {
+            if (index < _fullName.Length && index >= 0)
+                return _fullName[index];
+            return '\0';
+        } 
+    }
+
+}
